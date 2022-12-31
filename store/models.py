72c151d6fde4e20f098ae4b30095008abacbd7ba
devbobnwaka <nwakachibueze@gmail.com>
2022-12-31 20:09:49 +0100

    Get the instance of variation part 02

--- store/models.py
+++ store/models.py
@@ -23,6 +23,12 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
 
 class Variation(models.Model):
     variation_category_choice = (
@@ -36,5 +42,7 @@ class Variation(models.Model):
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
 
-    def __unicode__(self):
-        return self.product
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
